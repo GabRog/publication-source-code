e4008a0708683437cf842cc5c0a13988aa607797
grogowsk <grogowsk@akamai.com>
2020-12-27 11:31:31 +0100

    new measurement

--- run_pmd.sh
+++ run_pmd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 for file in $(find ultra-hard/JSAT/JSAT/src/jsat -type f); do
-    $(../pmd-bin-6.29.0/bin/run.sh pmd -d ${file} -f csv -R pmd_all_rules.xml -cache cache_new >> results/csv/test.csv)
+    $(../pmd-bin-6.29.0/bin/run.sh pmd -d ${file} -f csv -R pmd_all_rules.xml -cache cache_new >> results/csv/measurement_from_2712.csv)
 done
+
+#sed '/\"Problem\",\"Package\",\"File\",\"Priority\",\"Line\",\"Description\",\"Rule set\",\"Rule\"/d' -i results/csv/measurement_from_2712.csv
+# | sed  -i '1i "Problem","Package","File","Priority","Line","Description","Rule set","Rule"'
 
 
