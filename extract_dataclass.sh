25fcdcf4627e2cd2235d18027b7dce0190af00fa
grogowsk <grogowsk@akamai.com>
2020-12-27 09:11:57 +0100

    data class metrics measurement added

--- extract_dataclass.sh
+++ extract_dataclass.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #(WOC=27.273%, NOPA=0, NOAM=6, WMC=12)","Design","DataClass"
-awk -F, '/\"GodClass\"/' results/csv/test.csv |
-awk 'BEGIN{FS="\",\"";OFS=","} \
-    BEGIN{print "Package,Path,Line,WMC,ATFD,TCC"} \
-    {split($3,path,"JSAT\/JSAT"); $3=path[2]} \
-    {split($6,metric,"=")} \
-    {split(metric[2],wmc,",")} \
-    {split(metric[3],atfd,",");} \
-    {split(metric[4],tcc,"%");} \
-    {print $2,$3,$5,wmc[1],atfd[1],tcc[1]}' \
-> WMC_ATFD_TCC.csv
+
+gawk -F, '/,"DataClass"/' results/csv/test.csv |
+gawk 'BEGIN{FS="\",\"";OFS=","} \
+    BEGIN{print "Package,File,Line,ClassName,WOC,NOPA,NOAM,WMC"} \
+    {split($3,path,"JSAT/JSAT/src/jsat")} \
+    {match($6,/'\''(.*)'\'' (.*) \(WOC=(.*)%, NOPA=(.*), NOAM=(.*), WMC=(.*)\)/,metrics)} \
+    {woc=metrics[3]; nopa=metrics[4]; noam=metrics[5];  wmc=metrics[6]}
+    {print $2,path[2],$5,metrics[1],woc,nopa,noam,wmc}' \
+> DataClassMetrics.csv
